f50c940cd69019d7d1b329fa134ce6de38018819
Max0971 <nidzekostiantyn@gmail.com>
2025-06-22 21:29:33 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,3 +21,13 @@ def main():
 
 if __name__ == "__main__":
     main()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def health_check():
+    return "Bot is running", 200
+
+if __name__ == '__main__':
+    # Запускаємо Flask на порту 10000
+    app.run(host='0.0.0.0', port=10000)
