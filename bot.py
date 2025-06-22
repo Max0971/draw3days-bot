8e4e0c18cb727c342dd3f056348117b2be52c70c
Max0971 <nidzekostiantyn@gmail.com>
2025-06-22 15:53:29 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,17 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = "7240793453:AAFu5f4ArOokx2knYlF8JLoSJFbc0tO8WvU"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Бот активний! ✅")
+
+def main():
+    application = Application.builder().token(TOKEN).build()
+    
+    application.add_handler(CommandHandler("start", start))
+    
+    application.run_polling()
+
+if __name__ == "__main__":
+    main()
